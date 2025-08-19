7056ca6f6dccd712c86ffb3ae03062a3598ac6e3
Dmytro Dobrovolskiy <dimadobrovolski@gmail.com>
2025-08-19 05:41:00 +0300

    active class, ece section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,21 +4,44 @@ import cls from "./Header.module.css";
 
 export function Header() {
     return (
-        <div className="container">
-            <header className={cls.header}>
-                <nav className={cls.linksPages}>
-                    <NavLink to="/shop">Shop</NavLink>
-                    <NavLink to="/about">About us</NavLink>
-                    <NavLink to="/contacts">Contacts</NavLink>
-                </nav>
-                <NavLink to="/" className={cls.logo}>
-                    <h1>warm heart</h1>
-                </NavLink>
-                <nav className={cls.linksSearch}>
-                    <a href="">Search</a>
-                    <a href="">Cart (4)</a>
-                </nav>
-            </header>
-        </div>
+        <header>
+            <div className="container">
+                <div className={cls.header}>
+                    <nav className={cls.linksPages}>
+                        <NavLink
+                            to="/shop"
+                            className={({ isActive }) =>
+                                isActive ? `${cls.active}` : ""
+                            }
+                        >
+                            Shop
+                        </NavLink>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                isActive ? `${cls.active}` : ""
+                            }
+                        >
+                            About us
+                        </NavLink>
+                        <NavLink
+                            to="/contacts"
+                            className={({ isActive }) =>
+                                isActive ? `${cls.active}` : ""
+                            }
+                        >
+                            Contacts
+                        </NavLink>
+                    </nav>
+                    <NavLink to="/" className={cls.logo}>
+                        <h1>warm heart</h1>
+                    </NavLink>
+                    <nav className={cls.linksSearch}>
+                        <a href="">Search</a>
+                        <a href="">Cart (4)</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
     );
 }
