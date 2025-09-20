59f1bf7e5cab3d20235ff5391bea3f53c4e9698a
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2025-09-20 05:01:03 +0300

    fix mobile scroll

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -52,10 +52,20 @@ export function Search({ onClose }) {
     }
 
     useEffect(() => {
-        document.body.style.overflow = "hidden";
+        const scrollY = window.scrollY;
+
+        document.body.style.position = "fixed";
+        document.body.style.top = `-${scrollY}px`;
+        document.body.style.left = "0";
+        document.body.style.right = "0";
 
         return () => {
-            document.body.style.overflow = "";
+            document.body.style.position = "";
+            document.body.style.top = "";
+            document.body.style.left = "";
+            document.body.style.right = "";
+
+            window.scrollTo(0, scrollY); // вернуть скролл на место
         };
     }, []);
 
